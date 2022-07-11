e03fc48805176636a78e11a91277217b72505736
Emerson Millard <emillard.code@gmail.com>
2022-07-11 09:12:53 -0700

    Initial commit

---
+++ src/main/java/com/project/springsecuritycustomcredentials/SpringSecurityCustomCredentialsApplication.java
@@ -0,0 +1,15 @@
+package com.project.springsecuritycustomcredentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCustomCredentialsApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringSecurityCustomCredentialsApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/project/springsecuritycustomcredentials/SpringSecurityCustomCredentialsApplicationTests.java
@@ -0,0 +1,8 @@
+package com.project.springsecuritycustomcredentials;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCustomCredentialsApplicationTests {
+
+}
